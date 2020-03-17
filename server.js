8acbee91405cac8b6b883afdeb3672c700fa52f0
NinaPajonk <50745190+NinaPajonk@users.noreply.github.com>
2020-03-17 11:28:14 +0100

    movie opdracht

--- server.js
+++ server.js
@@ -4,39 +4,87 @@ const port = 3000;
 const path = require('path');
 const slug = require('slug');
 const bodyParser = require('body-parser');
-const multer  = require('multer');
+const cc = require('camelcase');
+const multer = require('multer');
 
+//MongoDB
+const mongo = require('mongodb')
+require('dotenv').config()
 
-// object aanmaken
+// Database
+let db = null
+let url = 'mongodb+srv://NPajonk:Nina%40pajonk25@cluster0-ofs74.mongodb.net/test?authSource=admin&replicaSet=Cluster0-shard-0&readPreference=primary&appname=MongoDB%20Compass&ssl=true'
+
+mongo.MongoClient.connect(url, function(err, client){
+  if (err) {
+  throw err
+  }
+  db = client.db(process.env.DB_NAME)
+})
+
+//object aanmaken
 const data = {
-  persoon: 'Nina',
-  leeftijd: 20,
-  gender: 'vrouw',
-};
+  id: 'notebook',
+  title: 'Notebook',
+  genre: 'Romantic Drama',
+  description: ''
+}
+var upload = multer({dest: 'static/upload/'})
 
+// code uit slides backend
 express();
 app.use(bodyParser.urlencoded({ extended: true }));
-// app.get('/', movies);
+app.get('/', movies);
 app.post('/', add);
 app.get('/add', form);
 app.get('/id:, movie');
 app.set('view enigine', 'ejs');
 app.set('views', 'view');
 app.use('/static', express.static('static'));
+app.delete('/:id', remove)
+
+// movie opdracht code examples
+function movies(req, res){
+  res.render('list.ejs',{data: data})
+}
+
+function movie(req, res, next) {
+  var id = req.params.id
+  var movie = find(data, function (value) {
+    return value.id === id
+  })
+
+  if (!movie) {
+    next()
+    return
+  }
+
+  res.render('detail.ejs', {data: movie})
+}
 
 function form(req, res) {
-  res.render('add.ejs');
+  res.render('add.ejs')
 }
 
 function add(req, res) {
-  const id = slug(req.body.title).toLocaleLowerCase();
+  var id = slug(req.body.title).toLowerCase()
   data.push({
-    id,
+    id: id,
     title: req.body.title,
     plot: req.body.plot,
-    description: req.body.description,
-  });
-  res.redirect(`/${id}`);
+    description: req.body.description
+  })
+
+  res.redirect('/' + id)
+}
+function remove(req, res) {
+  var id = req.params.id
+
+  data = data.filter(function (value) {
+    return value.id !== id
+  })
+
+  res.json({status: 'ok'})
 }
 
   //home pagina
@@ -49,5 +97,5 @@ function add(req, res) {
 
   // route naar ejs. Renderen
   app.get('/', (req, res) => res.render('detail.ejs', { data }));
-  
+
   app.listen(port,() => console.log('Example app listening on port' + port));
